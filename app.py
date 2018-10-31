a655492b7d7618068da63273b231766f324ec684
Todd Waits <twaits@sei.cmu.edu>
2018-10-30 20:17:09 -0400

    Added Guild (1) -> Director (1) and Guild (1) -> Actor (1) one to one relationships

--- app.py
+++ app.py
@@ -12,20 +12,19 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 
-actors = db.Table('actors',
-    db.Column('actor_id', db.Integer, db.ForeignKey('actor.id')),
-    db.Column('movie_id', db.Integer, db.ForeignKey('movie.id'))
+actors = db.Table(
+    "actors",
+    db.Column("actor_id", db.Integer, db.ForeignKey("actor.id")),
+    db.Column("movie_id", db.Integer, db.ForeignKey("movie.id")),
 )
 
 
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255))
-    director_id = db.Column(db.Integer, db.ForeignKey('director.id'))
+    director_id = db.Column(db.Integer, db.ForeignKey("director.id"))
     release_date = db.Column(db.DateTime)
-    actors = db.relationship(
-        'Actor', secondary=actors, backref='movies', lazy="select"
-    )
+    actors = db.relationship("Actor", secondary=actors, backref="movies", lazy="select")
 
     def release_year(self):
         return self.release_date.strftime("%Y")
@@ -39,20 +38,31 @@ class Director(db.Model):
     first_name = db.Column(db.String(255))
     last_name = db.Column(db.String(255))
     movies = db.relationship(
-        "Movie", backref=db.backref("director", lazy="joined"), lazy="select")
+        "Movie", backref=db.backref("director", lazy="joined"), lazy="select"
+    )
+    guild = db.relationship(
+        "GuildMembership", backref="director", lazy="select", uselist=False
+    )
+
 
 # m = Movie(...)
 # m.director.first_name
+
 
 class Actor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(255))
     last_name = db.Column(db.String(255))
+    guild = db.relationship(
+        "GuildMembership", backref="actor", lazy="select", uselist=False
+    )
 
 
 class GuildMembership(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     guild = db.Column(db.String(255))
+    direcotr_id = db.Column(db.Integer, db.ForeignKey("director.id"))
+    actor_id = db.Column(db.Integer, db.ForeignKey("actor.id"))
 
 
 @app.route("/")
@@ -76,21 +86,38 @@ def bootstrap_data():
     db.create_all()
 
     m = Movie(
-            title="Evil Dead",
-            release_date=datetime.strptime("Oct 15 1981", "%b %d %Y")
-        )
+        title="Evil Dead", release_date=datetime.strptime("Oct 15 1981", "%b %d %Y")
+    )
 
     db.session.add(m)
-    d = Director(first_name="Sam", last_name="Raimi")
+    d = Director(
+        first_name="Sam", last_name="Raimi", guild=GuildMembership(guild="Raimi DGA")
+    )
     m.director = d
 
     db.session.add(d)
 
-    bruce = Actor(first_name="Bruce", last_name="Campbell")
-    ellen = Actor(first_name="Ellen", last_name="Sandweiss")
-    hal = Actor(first_name="Hal", last_name="Delrich")
-    betsy = Actor(first_name="Betsy", last_name="Baker")
-    sarah = Actor(first_name="Sarah", last_name="York")
+    bruce = Actor(
+        first_name="Bruce",
+        last_name="Campbell",
+        guild=GuildMembership(guild="Campbell SAG"),
+    )
+    ellen = Actor(
+        first_name="Ellen",
+        last_name="Sandweiss",
+        guild=GuildMembership(guild="Sandweiss SAG"),
+    )
+    hal = Actor(
+        first_name="Hal",
+        last_name="Delrich",
+        guild=GuildMembership(guild="Delrich SAG"),
+    )
+    betsy = Actor(
+        first_name="Betsy", last_name="Baker", guild=GuildMembership(guild="Baker SAG")
+    )
+    sarah = Actor(
+        first_name="Sarah", last_name="York", guild=GuildMembership(guild="York SAG")
+    )
 
     db.session.add(bruce)
     db.session.add(ellen)
